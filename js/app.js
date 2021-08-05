26d9bd8aeffdd15fd62693d0fbd9fa950145523e
Michael Moore <mmoore4361@gmail.com>
2021-08-05 10:49:22 +0200

    Add code to files

---
+++ js/app.js
@@ -0,0 +1,93 @@
+// Constructing global vars
+
+const navList = document.getElementById('navbar__list');
+const sections = document.querySelectorAll('section');
+
+const listName = [];
+const sectionID = [];
+
+let landingContainer = document.querySelectorAll('.landing__container');
+
+const listContainers = [...landingContainer];
+
+console.log(listContainers);
+
+
+
+for (let section of sections) {
+    let names = section.querySelectorAll('h2')[0].textContent;
+
+    listName.push(names);
+
+    sectionID.push(section.id);
+}
+
+// console.log(listName);
+// console.log(sectionID);
+
+// Function to build nav
+
+function nav() {
+    for (let x = 1; x <= sectionID.length; x++) {
+        const li = document.createElement("li");
+        li.classList = "list";
+        li.id = `${sectionID[x - 1]}Navbar`;
+        navList.appendChild(li);
+
+        li.innerHTML = `<a class="anchor">${listName[x - 1]}</a>`;
+
+        li.addEventListener('click', function(event) {
+            event.preventDefault();
+            sections[x - 1].scrollIntoView({ behavior: "smooth" });
+        });
+    };
+};
+
+
+// Function for determining viewport
+
+function scroll(event) {
+    const position = event.getBoundingClientRect();
+    const wh = window.innerHeight;
+    const ww = window.innerWidth;
+    return (
+        position.top >= 0 &&
+        position.left >= 0 &&
+        position.right <= (ww || document.documentElement.clientWidth) &&
+        position.bottom <= (wh || document.documentElement.clientHeight)
+    );
+};
+
+// Function for isActive added to sections
+
+function active() {
+    for (let list of listContainers) {
+        let toggle = list.parentElement.id;
+        let navList = document.getElementById(`${toggle}Navbar`)
+        window.addEventListener('scroll', () => {
+            if (scroll(list)) {
+                list.classList.add('isActive');
+                navList.classList.add('active');
+            } else {
+                list.classList.remove('isActive');
+                navList.classList.remove('active');
+            }
+        });
+    }
+};
+
+// Add toTop button
+let toTop = document.getElementById("toTopBtn");
+
+window.addEventListener('scroll', () => {
+    if (window.pageYOffset > 100) {
+        toTop.classList.add("on");
+    } else {
+        toTop.classList.remove("on");
+    }
+});
+
+// Running my functions
+
+nav();
+active();
